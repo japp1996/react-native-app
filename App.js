7e65f7cfc91ea150febf6abfd3447eb84ec7019f
japp1996 <japp19961996@gmail.com>
2019-06-27 07:53:30 -0400

    Instalando plugins de la comunidad

--- App.js
+++ App.js
@@ -7,12 +7,13 @@
  */
 
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import LastMovieList from './src/videos/containers/last-movie-list';
 import API from './utils/api';
+import Video from 'react-native-video';
 
 type Props = {};
 export default class App extends Component {
@@ -32,6 +33,22 @@ export default class App extends Component {
     return (
       <Home>
         <Header/>
+        <Text>Pelicula Big Buck Bunny</Text>
+        <View style={{
+            flex: 1, 
+            height:100
+          }}>
+          <Video source={{uri:'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}} 
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>Buscador</Text>
         <Text>Categorias</Text>
         <LastMovieList list={this.state.lastMovieList}/>
