3f134dd31278ec6d31c82812bbf7979645ab39b4
Jose Angel Padron <japp19961996@gmail.com>
2019-06-26 22:20:58 -0400

    Separador Horizontal y BackgroundImage

---
+++ src/videos/components/last-movie-list-layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { 
+	View, 
+	Text, 
+	StyleSheet, 
+	ImageBackground,
+} from 'react-native';
+
+function LastMovieListLayout(props) {
+	return(
+		<ImageBackground source={require('../../../assets/background.jpg')} style={styles.container}>
+			<Text style={styles.title}>
+				{ props.title }
+			</Text>
+			{ props.children }
+		</ImageBackground>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		paddingVertical: 30,
+		paddingHorizontal: 10
+	},
+	title: {
+		color: '#4c4c4c',
+		fontSize: 20,
+		marginBottom: 10,
+		fontWeight: 'bold',
+	}
+})
+
+export default LastMovieListLayout
--- src/videos/containers/last-movie-list.js
+++ src/videos/containers/last-movie-list.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {FlatList,Text} from 'react-native';
 import Empty from '../components/empty';
-import Separator from '../components/vertical-separator';
+import Separator from '../../../src/sections/components/horizontal-separator';
 import Suggestion from '../components/suggestion';
+import Layout from '../components/last-movie-list-layout';
 
 class LastMovieList extends Component {
 	renderEmpty = () => <Empty text="No hay sugerencias"/>
@@ -15,13 +16,15 @@ class LastMovieList extends Component {
 	keyExtractor = (item) => item.id.toString() 
 	render() {
 		return (
-			<FlatList 
-				horizontal
-				keyExtractor={this.keyExtractor}
-				data={this.props.list} 
-				ListEmptyComponent={this.renderEmpty}
-				ItemSeparatorComponent={this.itemSeparator}
-				renderItem={this.renderItem} />
+			<Layout title="Categorias">
+				<FlatList 
+					horizontal
+					keyExtractor={this.keyExtractor}
+					data={this.props.list} 
+					ListEmptyComponent={this.renderEmpty}
+					ItemSeparatorComponent={this.itemSeparator}
+					renderItem={this.renderItem} />	
+			</Layout>			
 		)
 	}
 }
